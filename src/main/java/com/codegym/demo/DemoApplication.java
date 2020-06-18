b270e76613a6684557d78e8884e9e9bb4c2fe41c
linh-phamdang21 <ph.danglinh214@gmail.com>
2020-06-18 10:29:19 +0700

    Init project

---
+++ src/main/java/com/codegym/demo/DemoApplication.java
@@ -0,0 +1,15 @@
+package com.codegym.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.security.Security;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+    
+}
